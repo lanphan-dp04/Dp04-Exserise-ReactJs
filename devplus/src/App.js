7f3f89caa00a4b323bcae86a9a108b5c7554706e
NgoSinh <ngosinh0@gmail.com>
2022-11-09 16:49:00 +0700

    Feature Main_Typical done 70%, doing reponsive

--- devplus/src/App.js
+++ devplus/src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./app/stylesheet/style.scss"
-import './App.css';
-
+import "./app/stylesheet/style.scss";
+import "./App.css";
+import Typical from "./app/components/main/Typical/Typical";
 
 function App() {
   return (
-    <></>
+    <>
+      <Typical />
+    </>
   );
 }
 
