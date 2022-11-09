34c9e65f0340bd856afb6080bda3d3e5b796a424
NgoSinh <ngosinh0@gmail.com>
2022-11-09 22:08:19 +0700

    create source for Feature Main We

---
+++ devplus/src/app/components/main/mainDev/MainDev.jsx
@@ -0,0 +1,38 @@
+import "./MainDev.scss"
+
+import React from 'react'
+
+ function MainDev() {
+  return (
+    <div className="ss-dev">
+        <div className="container">
+            <div className="wpb_column">
+                <div className="vc_column-inner">
+                    <div className="wpb_wrapper">
+                        <div className="houzez-module module-title"> 
+                            <h2>WE DEVELOP | WE SUPPORT | WE IMPROVE</h2>
+                            <h3>Launch your ideas. Execute your plans. Maximize project success.</h3>
+                        </div>
+                        <div className="houzez-module module-service">
+                            <div className="row-no-margin">
+                                <div className="module-item">
+                                    <div className="service-block">
+                                        <div className="block-icon">
+                                            <img></img>
+                                        </div>
+                                        <div className="block-content">
+                                            <h3>Web App Development</h3>
+                                            <p>We create beautifully designed web apps precisely tailored to your situationWe deliver web systems of any size and complexity — from budding startup products to hi-end enterprise solutions.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default MainDev;
