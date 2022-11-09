3d3a3fc591d97eee7dd3c44f0accf3013c87b141
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-09 09:05:36 +0700

    creat resource base

---
+++ devplus/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./app/stylesheet/style.scss"
+import './App.css';
+
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
