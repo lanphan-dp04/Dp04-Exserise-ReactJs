53a998d46288b4e7184d80b608f6f67b283083f8
NgoSinh <ngosinh0@gmail.com>
2022-11-10 11:52:42 +0700

    fix import  Feature Main_Typical to Main.jsx

--- devplus/src/App.js
+++ devplus/src/App.js
@@ -2,14 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./app/stylesheet/style.scss";
 import "./App.css";
-import Typical from "./app/components/main/Typical/Typical";
 
 function App() {
-  return (
-    <>
-      <Typical />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
