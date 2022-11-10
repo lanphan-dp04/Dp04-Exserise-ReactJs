060e8213c278d613838507786392abaee67dce1a
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-10 14:49:30 +0700

    implement layout home page

--- devplus/src/App.js
+++ devplus/src/App.js
@@ -2,27 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./app/stylesheet/style.scss";
 import "./App.css";
-import Typical from "./app/components/main/Typical/Typical";
-import MainDev from "./app/components/main/mainDev/MainDev";
 import Footer from "./app/components/footer/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./app/stylesheet/style.scss"
-import './App.css';
 import Header from "./app/components/header/Header";
 import Main from "./app/components/main/Main";
 
 
 function App() {
   return (
-    <>
-      <Typical />
-      <MainDev />
-      <Footer />
+    <div className="app">
       <Header/>
       <Main/>
       <Footer />
-    </>
+    </div>
   );
 
 }
--- devplus/src/app/components/main/Main.jsx
+++ devplus/src/app/components/main/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Banner from './banner/Banner'
 import MainDev from './mainDev/MainDev'
 import Partners from './partners/Partners'
+import Typical from './Typical/Typical'
 
 const Main = () => {
   return (
     <main>
       <Banner/>
+      <Typical/>
       <MainDev />
       <Partners/>
     </main>
