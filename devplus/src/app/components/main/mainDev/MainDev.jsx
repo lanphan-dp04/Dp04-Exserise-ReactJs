1e06df6fb4581fee83145204f9bd1bed413a3a10
NgoSinh <ngosinh0@gmail.com>
2022-11-10 10:28:06 +0700

    done Feature Main Dev

--- devplus/src/app/components/main/mainDev/MainDev.jsx
+++ devplus/src/app/components/main/mainDev/MainDev.jsx
@@ -1,34 +1,60 @@
 import "./MainDev.scss"
-
-import React from 'react'
+import icon_1 from "./web-1.png"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
  function MainDev() {
+    
+  const [itemMainDev, setItemMainDev] = useState([]);
+  const [isLoad, setIsLoad] = useState(true);
+
+  let url = "https://62d16e83d4eb6c69e7dd4ff6.mockapi.io/mustHave";
+  async function fetchDataCampus() {
+    try {
+      setIsLoad(true);
+      let res = await axios.get(url, {});
+      let temp = await res.data;
+      setIsLoad(false);
+      setItemMainDev(temp);
+    } catch (err) {
+      console.log("Error: ", err.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchDataCampus(url);
+    }, []);
+
   return (
     <div className="ss-dev">
         <div className="container">
-            <div className="wpb_column">
-                <div className="vc_column-inner">
-                    <div className="wpb_wrapper">
-                        <div className="houzez-module module-title"> 
-                            <h2>WE DEVELOP | WE SUPPORT | WE IMPROVE</h2>
-                            <h3>Launch your ideas. Execute your plans. Maximize project success.</h3>
-                        </div>
+            <div className="vc_column-inner">
+                <div className="wpb_wrapper">
+                    <div className="houzez-module module-title text-center"> 
+                        <h2>WE DEVELOP | WE SUPPORT | WE IMPROVE</h2>
+                        <h3>Launch your ideas. Execute your plans. Maximize project success.</h3>
+                    </div>
                         <div className="houzez-module module-service">
                             <div className="row-no-margin">
-                                <div className="module-item">
-                                    <div className="service-block">
-                                        <div className="block-icon">
-                                            <img></img>
+                            {itemMainDev.map((item, index) =>{
+                                return(
+                                        <div className="module-item" key={index}>
+                                            <div className="service-block">
+                                                <div className="block-icon">
+                                                    <img src={item?.icon} alt=""></img>
+                                                </div>
+                                                <div className="block-content">
+                                                    <h3>{item?.name}</h3>
+                                                    <p>{item?.title}</p>
+                                                    <a href="#a" className="find">{item?.more}</a>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="block-content">
-                                            <h3>Web App Development</h3>
-                                            <p>We create beautifully designed web apps precisely tailored to your situationWe deliver web systems of any size and complexity — from budding startup products to hi-end enterprise solutions.</p>
-                                        </div>
-                                    </div>
-                                </div>
+                                )
+                            })}
+                            
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
