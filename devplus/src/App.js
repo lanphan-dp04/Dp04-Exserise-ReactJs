a8046c12498ddc284791e44126a0953ab800a400
unknown <longho964@gmail.com>
2022-11-10 08:54:35 +0700

    up footer done

--- devplus/src/App.js
+++ devplus/src/App.js
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./app/stylesheet/style.scss"
-import './App.css';
-
+import "./app/stylesheet/style.scss";
+import "./App.css";
+import Footer from "./app/components/footer/Footer";
+import SlideClient from "./app/components/main/slideClient/SlideClient";
 
 function App() {
   return (
-    <></>
+    <>
+      <Footer />
+    </>
   );
 }
 
