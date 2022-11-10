1eee8da76957550eb831276a51484c4c38f499ea
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-10 09:59:33 +0700

    implement header and banner

--- devplus/src/App.js
+++ devplus/src/App.js
@@ -1,12 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./app/stylesheet/style.scss"
 import './App.css';
+import Header from "./app/components/header/Header";
+import Main from "./app/components/main/Main";
 
 
 function App() {
   return (
-    <></>
+    <>
+      <Header/>
+      <Main/>
+    </>
   );
 }
 
---
+++ devplus/src/app/components/header/nav/Nav.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <a href="#/" className="nav-link">
+            HOME
+          </a>
+        </li>
+        <li className="nav-item we-do">
+          <a href="#/" className="nav-link">
+            WHAT WE DO
+          </a>
+          <ul className="sub-menu">
+            <li className="menu-item">
+              <a href="#/" className="menu-link">ST SOFTWARE</a>
+            </li>
+            <li className="menu-item">
+              <a href="#/" className="menu-link">ST DIGITAL</a>
+            </li>
+            <li className="menu-item">
+              <a href="#/" className="menu-link">ST DIGITAL</a>
+            </li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a href="#/" className="nav-link">
+            WHO WE ARE
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#/" className="nav-link">
+            PORTFOLIO
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#/" className="nav-link">
+            JOIN US
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#/" className="nav-link">
+            LET’S TALK
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
