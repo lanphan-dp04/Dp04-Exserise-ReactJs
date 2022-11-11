a02a118ded5b016ebe230385341fc2247cb41840
John Doe <johndoe@example.com>
2022-11-11 09:46:41 +0700

    st incubation done

--- devplus/src/views/STincubation/MainIncubation.jsx
+++ devplus/src/views/STincubation/MainIncubation.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import StIncubation from "../../app/components/stIncubation/StIncubation";
+import TypicalIncubation from "../../app/components/stIncubation/TypicalIncubation";
 
 const MainIncubation = () => {
   return (
     <main>
-      
+      <StIncubation />
+      <TypicalIncubation />
     </main>
-  )
-}
+  );
+};
 
-export default MainIncubation
+export default MainIncubation;
